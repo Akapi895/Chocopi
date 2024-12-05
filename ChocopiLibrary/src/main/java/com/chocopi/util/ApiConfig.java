54276f469f6d2bac3efb57fb5e4870f0ee7b37c7
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2024-12-05 14:12:22 +0700

    singleton

--- ChocopiLibrary/src/main/java/com/chocopi/util/ApiConfig.java
+++ ChocopiLibrary/src/main/java/com/chocopi/util/ApiConfig.java
@@ -3,7 +3,7 @@ package com.chocopi.util;
 public class ApiConfig {
     private static final String GOOGLE_BOOKS_API_KEY = "AIzaSyD1mN1IDTAPkW7CtLUqhWI7YA4_BMJw248";
     private static final String GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q=";
-    private static final String OPENAI_API_KEY = "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";
+    private static final String OPENAI_API_KEY = "";
     private static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
 
     private static ApiConfig instance;
